6725a469f847de87fce93c18596afec6fb611516
Amit Bar-Gil <amit217@yandex.com>
2022-10-22 19:57:46 +0300

    feat: Undefined route

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
-import * as cors from "cors";
-import * as bodyParser from "body-parser";
-import * as dotEnv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotEnv from "dotenv";
 dotEnv.config();
+
+import routeUndefined from "./helpers/routeUndefined.js";
+import router from "./routes/index.js";
 
 const { PORT = 3000 } = process.env;
 const server = express();
+
+server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.use(router);
+server.use("*", routeUndefined);
 
 server.listen(PORT);
