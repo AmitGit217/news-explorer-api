18f59e101a8643b06111eca15a9b1c9fd3014073
Amit Bar-Gil <amit217@yandex.com>
2022-10-23 12:46:23 +0300

    chore: article model

---
+++ article/article.schema.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { URL_REGEX } from "../lib/constants";
+
+const Article = new mongoose.Schema({
+  keyword: { type: String, required: true },
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  date: { type: String, required: true },
+  source: { type: String, required: true },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => URL_REGEX.test(value),
+      message: (props) => `${props.value} is not a valid URL`,
+    },
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (value) => URL_REGEX.test(value),
+      message: (props) => `${props.value} is not a valid URL`,
+    },
+  },
+  owner: { ref: "user", type: mongoose.Schema.Types.ObjectId, require: true },
+});
+
+export default mongoose.model("article", Article);
